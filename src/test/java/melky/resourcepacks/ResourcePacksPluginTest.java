3799cb4eb13549724e773d88d1e319ca8ef5eaf9
Ron Young <ron@ryoung.io>
2025-02-17 14:24:15 -0600

    hmm

--- src/test/java/melky/resourcepacks/ResourcePacksPluginTest.java
+++ src/test/java/melky/resourcepacks/ResourcePacksPluginTest.java
@@ -14,6 +14,7 @@ import net.runelite.client.externalplugins.ExternalPluginManager;
 import org.junit.Test;
 import org.junit.AfterClass;
 import org.junit.Rule;
+import org.junit.rules.TestWatcher;
 
 @Slf4j
 public class ResourcePacksPluginTest
